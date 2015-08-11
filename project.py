15de8e9bfe3f769ad918ea61173a2713431f6d1a
Nazar Kukarkin <nazarkukarkin@gmail.com>
2015-08-11 17:31:20 +0300

    any name of file

--- project.py
+++ project.py
@@ -1,22 +1,25 @@
 import os
 import shutil as sh
 
-name = 'project'
+
 
 _path='/Users/K/Documents/pr.directory'
 
+
 _list=os.listdir(path=_path)
+
+_list.remove('.DS_Store') 
 
 
 
 for i in _list:
-    if  name == i[0:len(name)]:
-       if i[len(name)+1:len(i)] in _list:
-           sh.move(_path + '/' + i , _path + '/' +i[len(name)+1:len(i)] )
+    if '.' in i:
+        name = i[0:i.index('.')]
+        if i[len(name)+1:len(i)] in _list:
+            sh.move(_path + '/' + i , _path + '/' +i[len(name)+1:len(i)] )
 
-       else:
-           os.mkdir(_path+'/'+i[len(name)+1:len(i)])
-           sh.move(_path + '/' + i , _path + '/' +i[len(name)+1:len(i)] )
+        else:
+            sh.move(_path + '/' + i , _path + '/' +i[len(name)+1:len(i)] )
  
             
 
