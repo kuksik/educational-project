7babceedd8adcbdc25492453188094db7079bafa
Nazar Kukarkin <nazarkukarkin@gmail.com>
2015-08-19 13:51:55 +0300

    treeSort, #2

--- project.py
+++ project.py
@@ -9,8 +9,6 @@ def fsort(inputStr):
      else:
          return('b')
         
-        
-
 
 def f(path):
 
@@ -19,30 +17,35 @@ def f(path):
     
     if '.DS_Store' in l:
         l.remove('.DS_Store')
-
-        
-
-    if l!=0:
-        
-        for i in l:
-            k=os.path.splitext(i)
-
-            if k[1]=='':
-                f(os.path.join(path,k[0]) )
-
-            else:
-                g=k[1][1:]
-
-                if g in l:
-                    os.rename(  os.path.join(path,i), os.path.join(path, g,i)  )
-                    
-                else:
-                    os.mkdir(  os.path.join(path,g )  )
-                    os.rename(  os.path.join(path, i) , os.path.join(path, g,i) )
-
-    else:
-        return()
+    print(path)
     
+    s=0
+    for i in l:
+         if os.path.splitext(i)[1][1:]==os.path.split(path)[1]:
+              s=s+1
+    print(len(l), s)
+
+    if len(l)!=s:
+         for i in l:
+              k=os.path.splitext(i)
+              g=k[1][1:]
+              if g!='':
+                   print(i)
+                   if g in l:
+                        os.rename( os.path.join(path,i), os.path.join(path,g,i) )
+                   else:
+                        os.mkdir(  os.path.join(path,g)  ) 
+                        os.rename(  os.path.join(path, i) , os.path.join(path, g,i) ) 
+                        
+
+              else:
+                   f(os.path.join(path,i))
+
+                        
+
+     
+               
+                 
 
 
 
