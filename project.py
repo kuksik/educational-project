c0083f7fd5f60251615fe3c11ceb842c8b7172cc
Nazar Kukarkin <nazarkukarkin@gmail.com>
2015-08-19 17:26:31 +0300

    treeSort#5

--- project.py
+++ project.py
@@ -1,9 +1,6 @@
 
 import os
 
-path_ = '/Users/K/Documents/pr.directory'
-
-
 
 def sort_list(inputStr):
 # Sorting the list: 1)files, 2)directories.
@@ -19,8 +16,10 @@ def sort_tree(path_):
     l = os.listdir(path_)
     l.sort(key=sort_list)
     
-    if '.DS_Store' in l: l.remove('.DS_Store')
+    # if '.DS_Store' in l: l.remove('.DS_Store')
+    # if '.localized' in l: l.remove('.localized')
     
+
     # To check whether need to sort directory.
     s = 0
     for i in l:
@@ -30,18 +29,19 @@ def sort_tree(path_):
  
 
     if s==1:
-        for i in l:
+        for i in l:   
             g=os.path.splitext(i)[1][1:]
 
             if g != '':
                 if g in l:
                     os.rename(os.path.join(path_,i), os.path.join(path_,g,i))
-            else:
-                os.renames(os.path.join(path_, i) , os.path.join(path_,g,i))                        
-    else:
-        f(os.path.join(path_,i))
+                else:
+                    os.renames(os.path.join(path_, i), os.path.join(path_,g,i))                        
 
+            else:
+                sort_tree(os.path.join(path_,i))
                         
 
-sort_tree(path_)
+
+sort_tree('/Users/K/Downloads/')
 
