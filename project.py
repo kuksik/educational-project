1f909243c77d8161945b1c8c67963532014a93b8
Nazar Kukarkin <nazarkukarkin@gmail.com>
2015-08-19 15:09:18 +0300

    treeSort

--- project.py
+++ project.py
@@ -2,7 +2,7 @@ import os
 
 path='/Users/K/Documents/pr.directory'
 
-
+#sorting the list: 1)files, 2)directories
 def fsort(inputStr):
      if '.' in inputStr:
         return 'a'
@@ -17,38 +17,31 @@ def f(path):
     
     if '.DS_Store' in l:
         l.remove('.DS_Store')
-    print(path)
     
+#to check whether need to sort directory
+
     s=0
     for i in l:
-         if os.path.splitext(i)[1][1:]==os.path.split(path)[1]:
-              s=s+1
-    print(len(l), s)
+         if os.path.splitext(i)[1][1:]!=os.path.split(path)[1]:
+              s=1
+              break
+ 
 
-    if len(l)!=s:
+    if s==1:
          for i in l:
-              k=os.path.splitext(i)
-              g=k[1][1:]
+
+              g=os.path.splitext(i)[1][1:]
+
               if g!='':
-                   print(i)
                    if g in l:
                         os.rename( os.path.join(path,i), os.path.join(path,g,i) )
+ 
                    else:
-                        os.mkdir(  os.path.join(path,g)  ) 
-                        os.rename(  os.path.join(path, i) , os.path.join(path, g,i) ) 
-                        
-
+                        os.renames(  os.path.join(path, i) , os.path.join(path,g,i) )                        
               else:
                    f(os.path.join(path,i))
 
                         
-
-     
-               
-                 
-
-
-
 
 f(path)
 
