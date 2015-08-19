75b75ebb3b3ddb550c338ac91338ff20271f682b
Nazar Kukarkin <nazarkukarkin@gmail.com>
2015-08-19 16:58:04 +0300

    treeSort#3

--- project.py
+++ project.py
@@ -1,47 +1,47 @@
+
 import os
 
-path='/Users/K/Documents/pr.directory'
+path_ = '/Users/K/Documents/pr.directory'
 
-#sorting the list: 1)files, 2)directories
-def fsort(inputStr):
-     if '.' in inputStr:
+
+
+def sort_list(inputStr):
+# Sorting the list: 1)files, 2)directories.
+
+    if '.' in inputStr:
         return 'a'
-     else:
-         return('b')
+    else:
+        return('b')
         
 
-def f(path):
+def sort_tree(path):
 
-    l = os.listdir(path)
-    l.sort(key=fsort)
+    l = os.listdir(path_)
+    l.sort(key=sortlist)
     
-    if '.DS_Store' in l:
-        l.remove('.DS_Store')
+    if '.DS_Store' in l: l.remove('.DS_Store')
     
-#to check whether need to sort directory
-
-    s=0
+    # To check whether need to sort directory.
+    s = 0
     for i in l:
-         if os.path.splitext(i)[1][1:]!=os.path.split(path)[1]:
-              s=1
-              break
+        if os.path.splitext(i)[1][1:] != os.path.split(path)[1]:
+            s = 1
+            break
  
 
     if s==1:
-         for i in l:
+        for i in l:
+            g=os.path.splitext(i)[1][1:]
 
-              g=os.path.splitext(i)[1][1:]
-
-              if g!='':
-                   if g in l:
-                        os.rename( os.path.join(path,i), os.path.join(path,g,i) )
- 
-                   else:
-                        os.renames(  os.path.join(path, i) , os.path.join(path,g,i) )                        
-              else:
-                   f(os.path.join(path,i))
+            if g != '':
+                if g in l:
+                    os.rename(os.path.join(path,i), os.path.join(path,g,i))
+            else:
+                os.renames(os.path.join(path, i) , os.path.join(path,g,i))                        
+    else:
+        f(os.path.join(path,i))
 
                         
 
-f(path)
+sort_tree(path)
 
