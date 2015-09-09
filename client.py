6c41997eaef6b4216e51f5cecab3237a64ec5d83
kuksik <nazarkukarkin@gmail.co>
2015-09-09 16:48:07 +0300

    copy and sort

--- client.py
+++ client.py
@@ -2,53 +2,27 @@ import socket
 
 import os
 
+from sorttree import client_send
 
-host = '127.0.0.1'
-port = 9091
-drctr_path = "/Users/K/Documents/exp"
+
+print('enter server ip')
+host = input()
+# host = '127.0.0.1'
+
+print('enter port number')
+port = int(input())
+# port = 9091
+
+print('enter the path to the folder which you want to copy')
+drctr_path = input()
+# drctr_path = "/Users/K/Documents/client"
 
     
 sock = socket.socket()
 sock.connect((host, port))
 
-list_dir = os.listdir(drctr_path)
 
-if '.DS_Store' in list_dir:
-    list_dir.remove('.DS_Store')
-
-sock.send(str(len(list_dir)).encode('utf-8'))
-sock.recv(51)
-
-
-
-for el in list_dir:
-
-    el_path = os.path.join(drctr_path, el)
-
-    if len(el.split('.')) == 2:
-
-        sock.send(b'file')
-
-            
-        file_size = os.path.getsize(el_path) 
-        sock.send(str(file_size).encode('utf-8'))
-        sock.recv(75)
-
-        file_name = os.path.basename(el_path)
-        sock.send(str(file_name).encode('utf-8'))
-        sock.recv(75)
-
-        file = open(el_path)
-        file_data = file.read()
-        sock.send(file_data.encode('utf-8'))
-
-        file.close
-    
-
-    else:
-        sock.send(b'drct')
-        sock.send(str(el).encode('utf-8'))
-        sock.recv(81)
+client_send(drctr_path, sock)
 
 
 sock.close()
--- server.py
+++ server.py
@@ -2,58 +2,38 @@ import socket
 
 import os
 
-import sys
+from sorttree import server_recv
 
-host = '127.0.0.1'
-port = 9091
-path_save = '/Users/K/Documents/123'
+from sorttree import sort_tree
+
+
+print('enter ip server')
+host = input()
+# host = '127.0.0.1'
+
+print('enter port number')
+port = int(input())
+# port = 9091
+
+print('enter the path to the folder in which the files will be saved')
+path_save = input()
+# path_save = "/Users/K/Documents/server"
+
 
 sock = socket.socket()
 sock.bind((host, port))
 sock.listen(1)
 conn, addr = sock.accept()
 
-list_dir_len = conn.recv(10)
-list_dir_len = int(list_dir_len.decode('utf-8'))
-conn.send(b"ok")
-# print(list_dir_len)
 
-for el in range(list_dir_len):
-
-    file_or_drctr = conn.recv(53)
-    file_or_drctr.decode('utf-8')
-    # print(str(file_or_drctr), type(file_or_drctr))
-
-    if file_or_drctr == "file":
-    
-        file_size = conn.recv(10)
-        file_size = int(file_size.decode('utf-8'))
-        # print('file size = ', file_size)
-        conn.send(b'the file size has received')
-
-        file_name = conn.recv(30)
-        file_name = str(file_name.decode('utf-8'))
-        # print('file name is', file_name)
-        conn.send(b'the file name has received')            
-        
-        path_ = os.path.join(path_save, file_name)
-        file_data = conn.recv(file_size)
-        file = open(path_, 'w')
-        file.write(file_data.decode('utf-8'))
-        file.close()
-
-    else:
-        drctr_name = conn.recv(20)
-        conn.send(b'the name  of folder has received')
-        drctr_name = str(drctr_name.decode("utf-8"))
-        # print('directories name is', drctr_name)
-        path_ = os.path.join(path_save, drctr_name)
-        os.mkdir(path_)
+server_recv(path_save, conn)
 
         
 sock.close()
 conn.close()
 
+
+sort_tree(path_save)
 
 
 
